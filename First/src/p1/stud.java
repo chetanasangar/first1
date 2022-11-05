706b42d5d631dfbe8809744c015563597c80f26c
Acer <Acer@DESKTOP-JUV82FV>
2022-11-06 00:15:15 +0530

    Pushing code first time.

---
+++ First/src/p1/stud.java
@@ -0,0 +1,21 @@
+package p1;
+
+public class stud
+{
+  int rollno ,age ; 
+ public void ravi()
+{
+	System.out.println("Welcome to all of you");
+	System.out.println("Automation is very easy");
+	}
+public static void main (String[] args)
+ {
+	stud abc= new stud();
+	abc.ravi();
+	abc.ravi();
+	int rollno= 4;
+	System.out.println(rollno);
+	int age =15;
+	System.out.println(age);
+   }
+}
